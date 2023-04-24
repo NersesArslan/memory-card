1ed23108b93f524d002456d4c43bbf810c05fc59
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-04-24 07:18:23 -0700

    working on card components, adding images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "../styles/cards.css";
+import image from "../pictures/20211108_165336.jpg";
 
 const Cards = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="card" id="1">
+        <img src={image} alt="image" />
+      </div>
+      <div className="card" id="2"></div>
+      <div className="card" id="3"></div>
+      <div className="card" id="4"></div>
+      <div className="card" id="5"></div>
+      <div className="card" id="6"></div>
+      <div className="card" id="7"></div>
+      <div className="card" id="8"></div>
+      <div className="card" id="9"></div>
+    </div>
+  );
 };
 
 export default Cards;
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-
+import "../styles/scoreboard.css";
 const Scoreboard = () => {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+
   return (
-    <div>
-      <p>Current Score: {score}</p>
-      <p>Best Score: {bestScore}</p>
+    <div className="scoreboard">
+      <h3>Current Score: {score}</h3>
+      <h3>Best Score: {bestScore}</h3>
     </div>
   );
 };
