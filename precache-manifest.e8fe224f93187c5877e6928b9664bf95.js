e569c0e4847497d30577b525cd9f2bcff644965a
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-05-06 13:25:10 -0700

    Updates

---
+++ precache-manifest.e8fe224f93187c5877e6928b9664bf95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "78a0b122f5e6890323c7",
+    "url": "/memory-card/static/css/main.dfb4a346.chunk.css"
+  },
+  {
+    "revision": "78a0b122f5e6890323c7",
+    "url": "/memory-card/static/js/main.e2a315d0.chunk.js"
+  },
+  {
+    "revision": "aacbb05a1a02806fa97c",
+    "url": "/memory-card/static/js/runtime~main.10382f37.js"
+  },
+  {
+    "revision": "35ee49a53b72299b6386",
+    "url": "/memory-card/static/js/2.6f844f57.chunk.js"
+  },
+  {
+    "revision": "eeb29d83d1bcbaba4e91",
+    "url": "/memory-card/static/js/3.82db40f9.chunk.js"
+  },
+  {
+    "revision": "5d60adece82012f35184d43083de3b08",
+    "url": "/memory-card/static/media/bergman.5d60adec.avif"
+  },
+  {
+    "revision": "51b91aca1e2c4386078ceb194aed4a5c",
+    "url": "/memory-card/index.html"
+  }
+];
